1dbc6d723f0d0227d8f24561e7c2783e734be813
Zxilly <zhouxinyu1001@gmail.com>
2024-03-22 22:11:58 +0800

    fix: add new symbol

--- embed.go
+++ embed.go
@@ -14,11 +14,26 @@ var readme string
 //go:embed *.go
 var goFiles embed.FS
 
-func init() {
+//go:noinline
+func UsingEmbedBytes() {
 	println(string(licenseBytes))
+}
+
+//go:noinline
+func UsingEmbedString() {
 	println(readme)
+}
+
+//go:noinline
+func UsingEmbedFS() {
 	files, _ := goFiles.ReadDir(".")
 	for _, file := range files {
 		println(file.Name())
 	}
+}
+
+func init() {
+	UsingEmbedBytes()
+	UsingEmbedString()
+	UsingEmbedFS()
 }
